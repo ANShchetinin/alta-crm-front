cf9de700de93fadacde35a00f6247608a62fa2d5
Alexander Shchetinin <anshchetinin@gmail.com>
2026-08-10 12:15:00 +0300

    Use import type for Client to fix Vite syntax error

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Client, getClients, createClient, updateClient, deleteClient } from '../api/clients';
+import type { Client } from '../api/clients';
+import { getClients, createClient, updateClient, deleteClient } from '../api/clients';
 
 export const Clients = () => {
   const { t } = useTranslation();
